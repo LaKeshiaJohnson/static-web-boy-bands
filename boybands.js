142ad8cd6708066771f3d125ba04de2ccc237ab9
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-07-18 15:21:05 -0500

    exercise

---
+++ boybands.js
@@ -0,0 +1,32 @@
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// The number of loops to perform (what if the array changes?)
+var loopCount = 5;
+
+// Keep track of which band we're on in the loop
+var currentBand = "";
+
+// Keep track of which veggie we're on in the loop
+var currentVeggie = "";
+
+// Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById("boy-bands");  /*Must be in quotes bc it's an ID */
+
+// Get a reference to the appropriate DOM element for vegetables
+var veggieElement = document.getElementById("vegetables");
+
+// Start looping
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+  
+  // Add the band names into the correct <div>
+  bandElement.innerHTML = bands; 
+
+  
+  // Add the veggie names into the correct <div>
+  veggieElement.innerHTML = vegetables;
+
+}
+
+/* bandElment is targeting the boy band div */
+/* innerHTML is putting the array inside the html document */
